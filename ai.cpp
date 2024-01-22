1ea41188cd71bfa2bf82d5bcd4acbfafbed95f1f
Zlatovlaska11 <marpecqueur@gmail.com>
2024-01-22 15:13:35 +0100

    finished ai for first move

--- ai.cpp
+++ ai.cpp
@@ -1,36 +1,56 @@
-#include "import.h"
+#include "ai_import.h"
+#include <algorithm>
 #include <string>
+#include <utility>
 
 using namespace std;
-class Ai {
+
+class Ai_Hard {
 private:
+  char *moves = new char[3];
   char board[3][3];
-  bool _IsFisrt;
-  int _round;
-
-  string Mover(int moveRow, int moveCol) {
-
-    if (_round == 1 && _IsFisrt) {
-      return "20";
-    }
-    return 0;
-  }
+  bool FirstMove;
 
 public:
-  Ai(bool isFirst, int round) {
-    this->_IsFisrt = isFirst;
-    this->_round = round;
+  Ai_Hard(string board) {
+    int x = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        this->board[i][j] = board[x];
+        x++;
+      }
+    }
   }
+  ~Ai_Hard() { free(moves); }
+
+  bool isFirstMove() {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[i][j] != '#') {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+    return true;
+  }
+
+  void SelectionOfMove() {
+    if (isFirstMove()) {
+      moves[0] = 'A';
+      moves[1] = '1';
+
+      moves[2] = '\0';
+    } else {
+    }
+  }
+  char *ReturnMove() { return moves; }
 };
 
-char board[3][3];
-bool _IsFisrt = true;
-int _round = 1;
-
-string Mover(int moveRow, int moveCol) {
-
-  if (_round == 1 && _IsFisrt) {
-    return "20";
-  }
+int main(int argc, char *argv[]) {
+  Ai_Hard hard("#########");
+  hard.SelectionOfMove();
+  cout << (string)hard.ReturnMove();
   return 0;
 }
