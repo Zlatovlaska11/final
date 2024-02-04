b27c0d12ce18bb8868c60e820a1a70f1f4ff3e16
Zlatovlaska11 <marpecqueur@gmail.com>
2024-02-04 12:58:55 +0100

    adding minimax for hard ai choice (still in implmentation)

--- ai.cpp
+++ ai.cpp
@@ -1,34 +1,43 @@
 #include "ai_import.h"
 #include <algorithm>
+#include <cstddef>
 #include <curses.h>
+#include <list>
 #include <string>
 #include <unistd.h>
 #include <utility>
-
 using namespace std;
 
-class Ai_Hard {
+class minmaxAi {
 private:
-  char *moves = new char[3];
   char board[3][3];
-  bool FirstMove;
+  char boardAfterAction[3][3];
 
-public:
-  Ai_Hard(string board) {
-    int x = 0;
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
-        this->board[i][j] = board[x];
-        x++;
+  list<string> moves;
+
+  bool isTerminalState(char player) {
+    for (int i = 0; i < 3; ++i) {
+      if ((board[i][0] == board[i][1] && board[i][1] == board[i][2] &&
+           board[i][0] == player) ||
+          (board[0][i] == board[1][i] && board[1][i] == board[2][i] &&
+           board[0][i] == player)) {
+        return true;
       }
     }
-  }
-  ~Ai_Hard() { delete[] moves; }
 
-  bool isFirstMove() {
+    // Check diagonals
+    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] &&
+         board[0][0] == player) ||
+        (board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
+         board[0][2] == player)) {
+      return true;
+    }
+    return false;
+  }
+  bool IsBoardFull() {
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-        if (board[i][j] == 'O') {
+        if (board[i][j] == '#') {
           return false;
         }
       }
@@ -36,22 +45,102 @@ public:
     return true;
   }
 
-  void SelectionOfMove() {
-    if (isFirstMove()) {
-      moves[0] = 'A';
-      moves[1] = '1';
-
-      moves[2] = '\0';
+  int eval() {
+    if (isTerminalState('X')) {
+      return 1;
+    } else if (isTerminalState('O')) {
+      return -1;
+    } else if (IsBoardFull()) {
+      return 0;
     } else {
+      return 100;
     }
   }
-  char *ReturnMove() { return moves; }
+
+  bool MaxingPlayerTurn() {
+    int x = 0;
+    int o = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[i][j] == 'X') {
+          x++;
+        } else if (board[i][j] == 'O') {
+          o++;
+        }
+      }
+    }
+    return x == o;
+  }
+
+  // void posibleMoves() {
+  //   int count = 0;
+  //   int sqare = 0;
+  //   for (int i = 0; i < 3; i++) {
+  //     for (int j = 0; j < 3; j++) {
+  //       sqare++;
+  //     }
+  //   }
+  //   for (int i = 0; i < 3; i++) {
+  //     for (int j = 0; j < 3; j++) {
+  //       sqare++;
+  //       if (board[i][j] == '#') {
+  //         moves = sqare;
+
+  //     }
+  //}
+  //}
+  //}
+  void posibleMoves() {
+    int arrSize = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[i][j] == '#') {
+          arrSize++;
+        }
+      }
+    }
+    int arrPosition = 0;
+    int sqare = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        sqare++;
+        if (board[i][j] == '#') {
+          string move = "";
+          move += i;
+          move += j;
+          moves.push_back(move);
+        }
+      }
+    }
+  }
+  void Result(string action, char player) {
+    if (board[action[0]][action[1]] == '#') {
+      boardAfterAction[action[0]][action[1]] = player;
+    } else {
+      throw "TF";
+    }
+  }
+
+public:
+  int MiniMax() {
+    if (eval() != 100) { // non terminal state
+      return eval();
+    }
+    return 0;
+  }
+
+  minmaxAi(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        this->board[i][j] = board[i][j];
+      }
+    }
+  }
 };
 
-string GetAiMove(string brd) {
-  Ai_Hard hard(brd);
-  hard.SelectionOfMove();
-  hard.isFirstMove();
-  sleep(1);
-  return hard.ReturnMove();
+int main(int argc, char *argv[]) {
+  char board[3][3] = {{'O', 'O', 'O'}, {'X', 'X', '#'}, {'#', '#', '#'}};
+  minmaxAi minmax(board);
+  cout << minmax.MiniMax();
+  return 0;
 }
