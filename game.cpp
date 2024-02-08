59834e9684847076c8c20122bf6c72ee7e49cd5e
Zlatovlaska11 <marpecqueur@gmail.com>
2024-02-08 17:51:53 +0100

    first functioning vesrion

--- game.cpp
+++ game.cpp
@@ -9,7 +9,178 @@
 #include <unistd.h>
 #include <utility>
 
+const std::string red("\033[0;31m");
+const std::string reset("\033[0m");
+
 using namespace std;
+
+class Settings {
+
+  struct ArrayWithColors {
+    std::string color;
+    char boardChar;
+  };
+
+  vector<string> GetWinnigPatter(char board[3][3], char winner) {
+    vector<string> Pattern;
+
+    int indexOfRow;
+    bool rowStreak = true;
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[i][j] == winner && rowStreak) {
+          rowStreak = true;
+          indexOfRow = i;
+        } else {
+          rowStreak = false;
+          break;
+        }
+      }
+      if (rowStreak) {
+        break;
+      }
+    }
+
+    int indexOfCol;
+    bool colStreak = true;
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (board[j][i] == winner && rowStreak) {
+          colStreak = true;
+          indexOfCol = i;
+        } else {
+          colStreak = false;
+          break;
+        }
+      }
+      if (colStreak) {
+        break;
+      }
+    }
+
+    if (rowStreak) {
+      for (int i = 0; i < 3; i++) {
+        string cordinates = to_string(indexOfRow);
+        cordinates.append(to_string(i));
+        Pattern.push_back(cordinates);
+      }
+    }
+    if (colStreak) {
+      for (int i = 0; i < 3; i++) {
+        string cordinates = to_string(i);
+        cordinates.append(to_string(indexOfCol));
+        Pattern.push_back(cordinates);
+      }
+    }
+
+    bool mainDiagonal = true;
+    for (int i = 0; i < 3; ++i) {
+      if (board[i][i] != winner) {
+        mainDiagonal = false;
+        break;
+      }
+    }
+
+    bool antiDiagonal = true;
+    for (int i = 0; i < 3; ++i) {
+      if (board[i][2 - i] != winner) {
+        antiDiagonal = false;
+        break;
+      }
+    }
+    if (mainDiagonal) {
+      for (int i = 0; i < 3; i++) {
+        string cordinates = to_string(i);
+        cordinates.append(to_string(i));
+        Pattern.push_back(cordinates);
+      }
+    }
+    if (antiDiagonal) {
+      for (int i = 0; i < 3; i++) {
+        string cordinates = to_string(i);
+        cordinates.append(to_string((2 - i)));
+        Pattern.push_back(cordinates);
+      }
+    }
+
+    return Pattern;
+  }
+
+public:
+  ArrayWithColors **ArrayHiglitedWin(char board[3][3], vector<string> pattern) {
+    string *ptrn = pattern.data();
+
+    ArrayWithColors **array = new ArrayWithColors *[3];
+
+    for (int i = 0; i < 3; ++i) {
+      array[i] = new ArrayWithColors[3];
+    }
+
+    int tmp = 0;
+    string hightliteCords = ptrn[0];
+
+    bool color = false;
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+
+        for (int k = 0; k < 3; k++) {
+          hightliteCords = ptrn[k];
+          if (hightliteCords[0] - '0' == i && hightliteCords[1] - '0' == j) {
+            color = true;
+          }
+        }
+        array[i][j] = {(color) ? red : reset, board[i][j]};
+        color = false;
+      }
+    }
+
+    return array;
+  }
+
+  void deleteColoredCharArray(ArrayWithColors **array, int rows) {
+    for (int i = 0; i < rows; ++i) {
+      delete[] array[i];
+    }
+    delete[] array;
+  }
+
+  int getConsoleWidth() {
+    struct winsize size;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+    return size.ws_col;
+  }
+
+  void PrintHiglitedBoard(char board[3][3], char player) {
+
+    int width = getConsoleWidth() / 2;
+
+    vector<string> coords = GetWinnigPatter(board, player);
+
+    ArrayWithColors **array = ArrayHiglitedWin(board, coords);
+
+    std::cout << string(width - 9, ' ') << "      1   2   3" << std::endl;
+    std::cout << string(width - 9, ' ') << "    ╔═══╦═══╦═══╗" << std::endl;
+    std::cout << string(width - 9, ' ') << " A  ║ " << array[0][0].color
+              << array[0][0].boardChar << reset << " ║ " << array[0][1].color
+              << array[0][1].boardChar << reset << " ║ " << array[0][2].color
+              << array[0][2].boardChar << reset << " ║" << std::endl;
+    std::cout << string(width - 9, ' ') << "    ╠═══╬═══╬═══╣" << std::endl;
+    std::cout << string(width - 9, ' ') << " B  ║ " << array[1][0].color
+              << array[1][0].boardChar << reset << " ║ " << array[1][1].color
+              << array[1][1].boardChar << reset << " ║ " << array[1][2].color
+              << array[1][2].boardChar << reset << " ║" << std::endl;
+    std::cout << string(width - 9, ' ') << "    ╠═══╬═══╬═══╣" << std::endl;
+    std::cout << string(width - 9, ' ') << " C  ║ " << array[2][0].color
+              << array[2][0].boardChar << reset << " ║ " << array[2][1].color
+              << array[2][1].boardChar << reset << " ║ " << array[2][2].color
+              << array[2][2].boardChar << reset << " ║" << std::endl;
+    std::cout << string(width - 9, ' ') << "    ╚═══╩═══╩═══╝" << std::endl;
+  }
+};
+
 class Game {
 private:
   char board[3][3] = {{'#', '#', '#'}, {'#', '#', '#'}, {'#', '#', '#'}};
@@ -302,7 +473,25 @@ public:
 
     endwin();
 
-    PrintHiglitedBoard(board, (round % 2 == 0) ? 'X' : 'O');
+    cout << "\033[2J\033[1;1H";
+
+    PrintLogo();
+
+    Settings set;
+
+    set.PrintHiglitedBoard(board, 'X');
+
+    initscr();
+
+    curs_set(0);
+
+    keypad(stdscr, TRUE);
+
+    cbreak();
+
+    getch();
+
+    endwin();
 
     cout << "\033[2J\033[1;1H";
 
--- import.h
+++ import.h
@@ -10,16 +10,4 @@ char *AiMove_easy();
 
 void CallWholeMenu();
 
-void PrintHiglitedBoard(char board[3][3], char player);
-
-struct ArrayWithColors {
-  std::string color;
-  char boardChar;
-};
-
 // Declaration for PrintHiglitedWin function
-ArrayWithColors **PrintHiglitedWin(char board[3][3],
-                                   std::vector<std::string> pattern);
-
-// Declaration for deleteColoredCharArray function
-void deleteColoredCharArray(ArrayWithColors **array, int rows);
